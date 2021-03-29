73226064b711eff3f44331c8df856740baf2dcdb
ByungSoo Ye <ybs680318@gmail.com>
2021-03-29 18:15:02 +0900

    초기세팅 완료

---
+++ my_settings.py
@@ -0,0 +1,14 @@
+SECRET_KEY = 'k0(a3^b7s%4@k4c@sty(haiim7rs+tta8!$r*#hqx9ylcks*hi'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'whatthe',
+        'USER': 'root',
+        'PASSWORD': 'dpqudtn14!',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+
+ALGORITHM = 'HS256'
