920cf701de68593053366a8cf179526672a4b532
ByungSoo Ye <ybs680318@gmail.com>
2021-03-31 10:43:37 +0900

    Modify: models.py 수정사항 반영 - URLField 삽입 - password 길이 늘리기 - 회원가입 마케팅 동의 null=True 제거 - 테이블명 단수 -> 복수 - 반복되는 comment 제거 (comment_like -> comment) - Actor 클래스 추가 - 평점 default 0

---
+++ user/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class User(models.Model):
+    name               = models.CharField(max_length=30)
+    email              = models.EmailField(max_length=254, unique=True)
+    password           = models.CharField(max_length=300)
+    profile_image_url  = models.URLField(max_length=3000, null=True)
+    maketing_is_agreed = models.BooleanField(default=False)
+    account_type       = models.ForeignKey('AccountType', on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        db_table = 'users'
+
+
+class AccountType(models.Model):
+    account_type = models.CharField(max_length=45)
+
+    class Meta:
+        db_table = 'account_types'
