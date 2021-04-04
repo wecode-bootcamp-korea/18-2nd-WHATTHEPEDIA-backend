7297c7cd41e98fbbf5561d965d3b8529d258a0e0
ByungSoo Ye <ybs680318@gmail.com>
2021-04-05 00:27:28 +0900

    Modify: 에러 코드 수정, 모델 import 두 줄로 나누기

--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.http      import JsonResponse
+from django.views     import View
 
-# Create your views here.
+from .models import Movie, MovieImage, Gallery, MovieGenre, Panel, UserStar, Comment
+        
+        
+class AutoCompleteView(View):
+    def get(self, request):
+        try:
+            query  = request.GET.get('query', None)
+            movies = Movie.objects.filter(korean_title__icontains=query) 
+            
+            query_list = [movie.korean_title for movie in movies]
+
+            return JsonResponse({'result': query_list}, status=200)
+        except ValueError:
+            return JsonResponse({'message': "Not a Valid Query"}, status=400)
+                
+
+class SearchView(View):
+    def get(self, request):
+        try: 
+            search_result = request.GET.get('query')
+            movie         = Movie.objects.get(korean_title=search_result)
+            movie_id      = movie.id
+
+            return JsonResponse({'result': movie_id}, status=200)
+        except Movie.DoesNotExist:
+            return JsonResponse({'message': 'Movie Does Not Exist'}, status=400)
+        
