68f52383486ced5f9c3555cc7185462fde7ec86f
InahChoi <hyejooworld@gmail.com>
2021-04-07 17:57:34 +0900

    [수정] 마이페이지 수정사항 반영

--- user/tests.py
+++ user/tests.py
@@ -1,9 +1,14 @@
-import unittest, json, requests
+import unittest, json, requests, jwt
+from django.http import response
 
-from django.test       import Client, TestCase
+from django.test       import Client, TestCase, client
 from unittest.mock     import patch, MagicMock
 
 from user.models       import User
+from movie.models      import (Category, Movie, MovieImage, MovieLike, UserStar)
+
+from .utils            import Authorization
+from my_settings       import SECRET_KEY, ALGORITHM
 
 class SignupTest(TestCase):
     def setUp(self):
@@ -161,3 +166,144 @@ class KakaoLoginTest(TestCase):
         response         = client.get('/user/login/kakao', content_type='application/json', **header)
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(), {'message' : 'KEY_ERROR'})
+
+class MyPageTest(TestCase):
+    def setUp(self):
+        client = Client()
+        Category.objects.create(
+            id   = 1,
+            name = '영화'
+        )
+        User.objects.create(
+            id                 = 1,
+            email              = 'whatthe@pe.dia',
+            name               = '최인아',
+            kakao_id           = '1004',
+            profile_image_url  = 'https://github.com/InahChoi/project_whatthe/blob/main/banner1.jpg?raw=true',
+        )
+        Movie.objects.create(
+            id                 = 1,
+            korean_title       = "장고",
+            english_title      = "Django",
+            release_date       = "2018-01-01",
+            nationality        = "미국",
+            number_of_audience = 25000,
+            description        = "django는 재밌엉",
+            running_time       = 130,
+            is_watcha          = False,
+            category_id        = 1,
+        )
+        MovieLike.objects.create(id=1, user_id=1, movie_id=1)
+
+        self.access_token = jwt.encode({'user_id' : User.objects.get(id=1).id}, SECRET_KEY, ALGORITHM)
+
+    def tearDown(self):
+        client = Client()
+        Category.objects.all().delete()
+        UserStar.objects.all().delete()
+        Movie.objects.all().delete()
+        User.objects.all().delete()
+
+    def test_mypage_success(self):
+        client          = Client()
+        user_star_count = UserStar.objects.filter(user_id=1, movie_id=1).count()
+        header          = {'HTTP_Authorization':self.access_token}
+        response        = client.get('/user/mypage', content_type='application/json', **header)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {
+            'mypage_data':
+            {
+                'user'            : '최인아',
+                'profile'         : 'https://github.com/InahChoi/project_whatthe/blob/main/banner1.jpg?raw=true',
+                'user_star_count' : user_star_count
+            }
+        })       
+
+    def test_mypage_not_found(self):
+        client   = Client()
+        header   = {'HTTP_Authorization':self.access_token}
+        response = client.get('/whatthe', content_type='application/json', **header)
+        self.assertEqual(response.status_code, 404)
+
+class MyEvaluationTest(TestCase):
+    def setUp(self):
+        client = Client()
+        Category.objects.create(
+            id   = 1,
+            name = '영화'
+        )
+        User.objects.create(
+            id                 = 1,
+            email              = 'whatthe@pe.dia',
+            name               = '최인아',
+            kakao_id           = '1004',
+            profile_image_url  = 'https://github.com/InahChoi/project_whatthe/blob/main/banner1.jpg?raw=true',
+        )
+        Movie.objects.create(
+            id                 = 1,
+            korean_title       = "장고",
+            english_title      = "Django",
+            release_date       = "2018-01-01",
+            nationality        = "미국",
+            number_of_audience = 25000,
+            description        = "django는 재밌엉",
+            running_time       = 130,
+            is_watcha          = True,
+            category_id        = 1,
+        )
+
+        Movie.objects.create(
+            id                 = 2,
+            korean_title       = "파이썬",
+            english_title      = "Python",
+            release_date       = "2021-02-07",
+            nationality        = "미국",
+            number_of_audience = 250000,
+            description        = "Python은 재밌엉",
+            running_time       = 100,
+            is_watcha          = True,
+            category_id        = 1,
+        )
+
+        MovieImage.objects.create(
+            id        = 1,
+            image_url = "https://github.com/InahChoi/project_whatthe/blob/main/banner2.jpg?raw=true",
+            movie_id  = 1
+            )
+
+        UserStar.objects.create(id=1, user_id=1, movie_id=1, star=5)
+
+        self.access_token = jwt.encode({'user_id' : User.objects.get(id=1).id}, SECRET_KEY, ALGORITHM)
+
+    def tearDown(self):
+        client = Client()
+        Category.objects.all().delete()
+        UserStar.objects.all().delete()
+        Movie.objects.all().delete()
+        MovieImage.objects.all().delete()
+        User.objects.all().delete()
+
+    def test_my_evaluation_success(self):
+        client      = Client()
+        user_star  = UserStar.objects.filter(user_id=1, movie_id=1)
+        header      = {'HTTP_Authorization':self.access_token}
+        response    = client.get('/user/rating', content_type='application/json', **header)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {
+            "movie_info_data": [
+                {
+                    "title": "장고",
+                    "image": "https://github.com/InahChoi/project_whatthe/blob/main/banner2.jpg?raw=true",
+                    "is_watcha": 1,
+                    "star": 5,
+                    "release_date": "2018-01-01"
+                }
+            ]
+        })    
+
+    def test_my_evaluation_not_found(self):
+        client   = Client()
+        header   = {'HTTP_Authorization':self.access_token}
+        response = client.get('/whatthe', content_type='application/json', **header)
+        self.assertEqual(response.status_code, 404)
+    
--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,11 @@
 from django.urls    import path
 
-from .views         import SignupView, LoginView, KakaoView
+from .views         import SignupView, LoginView, KakaoView, MyPageView, MyEvaluationView
 
 urlpatterns = [
     path('/signup', SignupView.as_view()),
     path('/login', LoginView.as_view()),
     path('/login/kakao', KakaoView.as_view()),
+    path('/mypage', MyPageView.as_view()),
+    path('/rating', MyEvaluationView.as_view()),
 ]
